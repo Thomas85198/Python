a1c84f3d03190e07f8470cb5a4c5f1171b89d46f
luthomas <Thomas.Lu@garmin.com>
2025-02-11 11:09:17 +0800

    167

--- 167. Intor to SQLite3.py
+++ 167. Intor to SQLite3.py
@@ -1,8 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect(":memory:")
+conn = sqlite3.connect("datafile.db")
 cursor = conn.cursor()
-cursor.execute("""
-create table people (id integer primary key, name text, count integer)
-""")
-cursor.execute("""insert into people (name, count) values ('Bob', 15)""")
+# cursor.execute("""
+# create table people (id integer primary key, name text, count integer)
+# """)
+# cursor.execute("""insert into people (name, count) values ('Bob', 15)""")
+
+# result = cursor.execute(
+#     "select * from people where name=:username", {"username": "Bob"})
+# print(result.fetchmany(2))
+
+cursor.execute("""update people set count = :usercount where name = :username""", {
+               "username": 'Bob', "usercount": 39})
+result = cursor.execute("select * from people")
+print(result.fetchall())
+
+conn.commit()
+conn.close()
